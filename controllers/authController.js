065df7a95564d85d54ac160891360a43bc9dc305
Elvis Inoa <josueinoa02@gmail.com>
2020-07-27 23:13:32 -0400

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+const Usuario = require('../models/Usuario');
+// const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+exports.autenticarUsuario = async(req, res) => {
+    // Revisar si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ) {
+        return res.status(400).json({errores: errores.array()})
+    }
+
+    // Extraer el email y el password
+    const { email, password } = req.body;
+
+    try {
+        // Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+        if(!usuario) {
+            return res.status(400).json({msg: 'El usuario no existe'});
+        }
+
+        // Revisar password
+        // const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        const passCorrecto = password;
+        if(!passCorrecto) {
+            return res.status(400).json({msg: 'Password Incorrecto'});
+        }
+
+        // Si todo es correcto crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmacion
+            res.json({ token });
+        }) 
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Obtiene que usuario esta autenticado
+exports.usuarioAutenticado = async (req, res) => {
+    try {
+        const usuario = await Usuario.findById(req.usuario.id).select('-password');
+        res.json(usuario);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({msg: 'Hubo un error'});
+    } 
+}
